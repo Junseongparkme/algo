c7093ab5cd98c69e25379a73f2be7ef4006109bd
Junseong Park <junseongpark.dev@gmail.com>
2023-01-23 12:03:46 +0900

    [Silver IV] Title: 좋은 단어, Time: 252 ms, Memory: 12728 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.js
@@ -0,0 +1,27 @@
+const input = require('fs')
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(v => v.trim())
+  .slice(1);
+
+let result = 0;
+
+for (let s of input) {
+  if (s.length % 2 === 1) continue;
+
+  let stack = [];
+
+  for (let c of s) {
+    if (stack.length === 0 || stack.at(-1) !== c) {
+      stack.push(c);
+    } else if (stack.at(-1) === c) {
+      stack.pop();
+    }
+  }
+
+  if (stack.length === 0) result++;
+}
+
+console.log(result);
