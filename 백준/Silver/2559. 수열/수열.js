7d6e3d97ee4e8fb7ff736c0238af6d2a88c2ad31
Junseong Park <junseongpark.dev@gmail.com>
2023-01-27 22:41:05 +0900

    [Silver III] Title: 수열, Time: 224 ms, Memory: 20316 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.js
+++ 백준/Silver/2559. 수열/수열.js
@@ -3,26 +3,23 @@ const input = require('fs')
   .toString()
   .trim()
   .split('\n')
-  .map(v =>
-    v
-      .trim()
-      .split(' ')
-      .map(v => +v)
-  );
+  .map(v => v.split(' ').map(v => +v));
 
-const [[n, k], temps] = input;
+const [N, K] = input[0];
+const temps = input[1];
+
 temps.unshift(0);
-let result = -1e9;
+const prefixSum = [0];
 
-let prefixSum = [0];
-
-for (let i = 1; i < temps.length; i++) {
-  prefixSum[i] = prefixSum[i - 1] + temps[i];
+for (let i = 1; i <= N; i++) {
+  const sum = prefixSum[i - 1] + temps[i];
+  prefixSum.push(sum);
 }
 
-for (let i = 0; i < prefixSum.length - k; i++) {
-  let unit = prefixSum[k + i] - prefixSum[i];
-  result = Math.max(result, unit);
+let maxTemp = -1e9;
+for (let i = K; i <= N; i++) {
+  const temp = prefixSum[i] - prefixSum[i - K];
+  maxTemp = Math.max(maxTemp, temp);
 }
 
-console.log(result);
+console.log(maxTemp);
