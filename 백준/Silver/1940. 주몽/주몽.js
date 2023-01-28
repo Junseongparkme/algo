af7d89abce842a0cf4c4cd841f5c9062dd291464
Junseong Park <junseongpark.dev@gmail.com>
2023-01-28 13:55:17 +0900

    [Silver IV] Title: 주몽, Time: 396 ms, Memory: 10572 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.js
+++ 백준/Silver/1940. 주몽/주몽.js
@@ -9,20 +9,10 @@ const [N, M] = input.slice(0, 2).map(v => +v);
 const list = input[2].split(' ').map(v => +v);
 let result = 0;
 
-function combination(start, b, r) {
-  if (b.length === r) {
-    let sum = b.reduce((acc, v) => acc + v, 0);
-    if (sum === M) result++;
-    return;
+for (let i = 0; i < N; i++) {
+  for (let j = i + 1; j < N; j++) {
+    if (list[i] + list[j] === M) result++;
   }
-
-  for (let i = start + 1; i < N; i++) {
-    b.push(list[i]);
-    combination(i, b, r);
-    b.pop();
-  }
-  return;
 }
 
-combination(-1, [], 2);
 console.log(result);
