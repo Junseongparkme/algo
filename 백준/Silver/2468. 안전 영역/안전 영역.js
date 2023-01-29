b85884cb49cd68f7762dd8a784598751f7409714
Junseong Park <junseongpark.dev@gmail.com>
2023-01-29 17:17:22 +0900

    [Silver I] Title: 안전 영역, Time: 332 ms, Memory: 22280 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.js
+++ 백준/Silver/2468. 안전 영역/안전 영역.js
@@ -3,74 +3,60 @@ const input = require('fs')
   .toString()
   .trim()
   .split('\n')
-  .map(v => v.trim());
+  .map(v =>
+    v
+      .trim()
+      .split(' ')
+      .map(v => +v)
+  );
 
-let [N, ...board] = input;
-let maxRain = 0;
-let result = 0;
+let N = input[0][0];
+let board = input.slice(1);
+let result = -1;
 
-N = +N;
-board = board.map(v => v.split(' ').map(v => +v));
+const dy = [-1, 0, 1, 0];
+const dx = [0, 1, 0, -1];
 
-for (let row of board) {
-  for (let value of row) {
-    maxRain = Math.max(maxRain, value);
-  }
-}
-
-for (let rain = 0; rain <= maxRain; rain++) {
-  // 침수배열 생성
-  let destroy = [];
+for (let rain = 0; rain <= 100; rain++) {
+  let state = [];
   let visited = [];
+  let area = 0;
 
   for (let i = 0; i < N; i++) {
-    let row = [];
+    let stateRow = [];
     let visitedRow = [];
+
     for (let j = 0; j < N; j++) {
-      row.push(false);
+      stateRow.push(board[i][j] - rain);
       visitedRow.push(false);
     }
-    destroy.push(row);
+
+    state.push(stateRow);
     visited.push(visitedRow);
   }
 
-  // 비가 내렸을때 침수배열 수정
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      if (board[i][j] <= rain) {
-        destroy[i][j] = true;
-      }
+      if (state[i][j] < 0) continue;
+      if (visited[i][j]) continue;
+      dfs(i, j);
+      area++;
     }
   }
 
-  // 침수되지 않은 영역 구하기
-  let aliveArea = 0;
-  const dy = [-1, 0, 1, 0];
-  const dx = [0, 1, 0, -1];
+  result = Math.max(result, area);
 
   function dfs(y, x) {
-    visited[y][x] = true;
+    visited[y][x] = 1;
     for (let i = 0; i < 4; i++) {
       let ny = y + dy[i];
       let nx = x + dx[i];
       if (!(0 <= ny && ny < N && 0 <= nx && nx < N)) continue;
+      if (state[ny][nx] < 0) continue;
       if (visited[ny][nx]) continue;
-      if (destroy[ny][nx]) continue;
       dfs(ny, nx);
     }
   }
-
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (destroy[i][j]) continue;
-      if (visited[i][j]) continue;
-      aliveArea++;
-      dfs(i, j);
-    }
-  }
-
-  // 침수되지 않은 영역이 최댓값인 경우 값으로 반환해야함.
-  result = Math.max(result, aliveArea);
 }
 
 console.log(result);
